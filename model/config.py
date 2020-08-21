386360c48ef508195f6eb03a2dee7abbf4bc0401
Michal Jablonski <michal.ttl@gmail.com>
2020-08-21 15:33:16 +0200

    Reduce read_offset_msec parameter in model/config.py

--- model/config.py
+++ model/config.py
@@ -20,4 +20,4 @@ spg_window_overlap_skip_pts = 128
 
 default_pyAudio_paFloat32_level_range = [1.0, -1.0]
 
-pyaudio_read_offset_msec = 140  # allows to manually adjust the input and output start synchronization for pyAudio dev
+pyaudio_read_offset_msec = 15  # allows to manually adjust the input and output start synchronization for pyAudio dev
